4af2ed1b35a9c1fea8f8035acdcb395539123ff6
loyivir <loyivir@gmail.com>
2021-04-09 20:03:28 +0300

    feat: Add advanced guessNumberGame

--- js/main.js
+++ js/main.js
@@ -2,26 +2,44 @@
 
 let guessNumber = function () {
   const randomNumber = Math.random().toFixed(2) * 100;
-console.log(randomNumber);
+  let tries = 10;
+  console.log(randomNumber);
   let guessPrompt = function () {
+    
     let numStr = prompt('Угадай число от 1 до 100');
-    console.log(numStr);
+    
+    if(tries === 1) {
+      if (confirm("Попытки закончились, хотите сыграть еще?")) {
+        guessNumber();
+      } else {
+        tries = 0;
+        return;
+      }
+    }else
     if (numStr === null) {
+      tries = 0;
       alert('Игра окончена');
       return;
     } else if (Number(numStr) === randomNumber) {
-      alert('Поздравляю, Вы угадали!!!');
-      return;
-    } else if (isNaN(parseInt(numStr))) {
+      if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')){
+       guessNumber();
+      } else {
+        tries = 0;
+        alert('Игра окончена');
+        return;
+      }      
+    } else if (isNaN(Number(numStr)) || numStr.trim() === '') {
       alert('Введи число!');
       guessPrompt();
     } else if (Number(numStr) > randomNumber) {
-      alert('Загаданное число меньше');
+      tries--;
+      alert('Загаданное число меньше, осталось попыток ' + tries);
       guessPrompt();
     } else if (Number(numStr) < randomNumber) {
-      alert('Загаданное число больше');
+      tries--;
+      alert('Загаданное число больше, осталось попыток ' + tries);
       guessPrompt();
-    }
+    } 
     
   };
   guessPrompt();
